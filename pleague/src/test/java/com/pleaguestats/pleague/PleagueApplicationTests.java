6b4732e63520840a3d445f73165798be770de474
dev_JosuehHenrique <josuehenrique2004@gmail.com>
2025-12-11 14:47:24 -0300

    commit

---
+++ pleague/src/test/java/com/pleaguestats/pleague/PleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleaguestats.pleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
